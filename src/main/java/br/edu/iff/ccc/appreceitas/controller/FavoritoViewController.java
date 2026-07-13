a3ca0587e5d4188532bf1e3eb686934c485dfe12
robson monteiro <133255385+robiinhowz@users.noreply.github.com>
2026-07-13 13:27:42 -0300

    Update FavoritoViewController.java

--- src/main/java/br/edu/iff/ccc/appreceitas/controller/FavoritoViewController.java
+++ src/main/java/br/edu/iff/ccc/appreceitas/controller/FavoritoViewController.java
@@ -3,18 +3,7 @@ package br.edu.iff.ccc.appreceitas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Controller responsável pela área logada de favoritos do usuário.
- *
- * Mapeia as rotas relacionadas a:
- * - RF08 (Sistema de Favoritos)
- * - RF09 (Remoção de Favoritos)
- * - RF10 (Área Logada)
- *
- * Nesta fase apenas a rota e a view são mapeadas. O controle de sessão,
- * autenticação e a lógica de negócio serão implementados em uma fase
- * posterior.
- */
+
 @Controller
 public class FavoritoViewController {
 
