dc3f0ddc41ba77794010ecbbea107fd24e9c07cd
Carloseduardoal10 <almeida.e@gsuite.iff.edu.br>
2026-07-13 00:48:13 -0300

    Fix: Ajustando sintaxe corrompida do AppReceitasApplication na TR03

--- src/main/java/br/edu/iff/ccc/appreceitas/AppReceitasApplication.java
+++ src/main/java/br/edu/iff/ccc/appreceitas/AppReceitasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class appreceitasApplication {
+public class AppReceitasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(appreceitasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppReceitasApplication.class, args);
+    }
 
 }
