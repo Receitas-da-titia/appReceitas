774e31bd9b1aa0aea79e689fbea7e1c6f259fe5e
robson monteiro <133255385+robiinhowz@users.noreply.github.com>
2026-07-13 13:27:17 -0300

    Update AutenticacaoViewController.java

--- src/main/java/br/edu/iff/ccc/appreceitas/controller/AutenticacaoViewController.java
+++ src/main/java/br/edu/iff/ccc/appreceitas/controller/AutenticacaoViewController.java
@@ -3,18 +3,7 @@ package br.edu.iff.ccc.appreceitas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Controller responsável pelas páginas de autenticação e cadastro de usuários.
- *
- * Mapeia as rotas relacionadas a:
- * - RF01 (Cadastro de Usuários)
- * - RF02 (Login de Usuários)
- * - RF13 (Logout do Sistema)
- *
- * Nesta fase apenas as rotas e as views são mapeadas. A lógica de
- * autenticação, validação e persistência será implementada em uma
- * fase posterior.
- */
+// Controller responsável pelas páginas de autenticação e cadastro de usuários
 @Controller
 public class AutenticacaoViewController {
 
