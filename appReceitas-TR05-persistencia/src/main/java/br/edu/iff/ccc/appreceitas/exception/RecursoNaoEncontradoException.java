0e556e2d00d195dd5f55f61f57b2ff6bd3282fbd
Robson Monteiro <robsonmonteiromachado@gmail.com>
2026-09-14 15:11:42 -0300

    Release v2.0.0

--- appReceitas-TR05-persistencia/src/main/java/br/edu/iff/ccc/appreceitas/exception/RecursoNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.iff.ccc.appreceitas.exception;
-
-public class RecursoNaoEncontradoException extends RuntimeException {
-    public RecursoNaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-    
-}
