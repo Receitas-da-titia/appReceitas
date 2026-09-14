65554f8707766df0babd01734aab84473ddd7197
robson monteiro <133255385+robiinhowz@users.noreply.github.com>
2026-09-14 14:55:35 -0300

    api rest

--- appReceitas-TR05-persistencia/src/main/java/br/edu/iff/ccc/appreceitas/exception/EntidadeDuplicadaException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.iff.ccc.appreceitas.exception;
-
-public class EntidadeDuplicadaException extends RuntimeException {
-    public EntidadeDuplicadaException(String mensagem) {
-        super(mensagem);
-    }
-    
-}
--- appReceitas-TR05-persistencia/src/main/java/br/edu/iff/ccc/appreceitas/exception/RegraDeNegocioException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.iff.ccc.appreceitas.exception;
-
-public class RegraDeNegocioException extends RuntimeException {
-    public RegraDeNegocioException(String mensagem) {
-        super(mensagem);
-    }
-    
-}
